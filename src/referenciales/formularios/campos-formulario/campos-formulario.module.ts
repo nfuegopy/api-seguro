c1e6940f2bb286c03061fdbc9cbf9853f2d56031
nfuegopy <acbleguizamon@gmail.com>
2025-10-28 20:24:40 -0300

    Formulario y endpoinst para campos formulario

--- src/referenciales/formularios/campos-formulario/campos-formulario.module.ts
+++ src/referenciales/formularios/campos-formulario/campos-formulario.module.ts
@@ -1,9 +1,14 @@
+/* eslint-disable prettier/prettier */
+
 import { Module } from '@nestjs/common';
-import { CamposFormularioController } from './campos-formulario.controller';
 import { CamposFormularioService } from './campos-formulario.service';
+import { CamposFormularioController } from './campos-formulario.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CampoFormulario } from './entities/campo-formulario.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CampoFormulario])],
   controllers: [CamposFormularioController],
-  providers: [CamposFormularioService]
+  providers: [CamposFormularioService],
 })
 export class CamposFormularioModule {}
